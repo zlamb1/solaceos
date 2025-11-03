d4b94cb70494f27ba1c476cc2213bbfc4fc2ee45
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2025-11-02 21:33:04 -0600

    added include dir and retrofitted build.py to use flags instead of purely positional args

--- build.py
+++ build.py
@@ -4,69 +4,131 @@ import os
 import subprocess
 import sys
 
-args = sys.argv
-
-if len(args) < 9:
-    sys.exit(1)
+TMP = 'tmp'
 
 # in terms of MB
-size = 64
-offset = 1
-offset_s = str(offset)
+SIZE = 64
+OFFSET = 1
+OFFSET_S = str(OFFSET)
 
-parted = args[1]
-limine = args[2]
-mcopy = args[3]
-mmd = args[4]
-limine_bios_sys = args[5]
-limine_conf = args[6]
-kernel_elf = args[7]
-output_img = args[8]
+class flag:
+    def __init__(self, long_name, default_value=None):
+        self.long_name = long_name
+        self.value = default_value
 
-tmp_img = 'tmp.img'
+flags = {
+    'parted': flag('parted', 'parted'),
+    'limine': flag('limine', 'limine'),
+    'mcopy': flag('mcopy', 'mcopy'),
+    'mmd': flag('mmd', 'mmd'),
+    'limine-bios-sys': flag('limine-bios-sys', 'limine-bios.sys'),
+    'limine-conf': flag('limine-conf', 'limine.conf'),
+    'output': flag('output', 'solace.img')
+}
 
-output_file = open(output_img, 'wb')
-output_file.seek(1024 * 1024 * offset)
-output_file.write(b'\0')
-output_file.close()
+def usage():
+    print(f'{args[0]} [OPTIONS] [KERNEL]', file=sys.stderr)
+    sys.exit(1)
 
-tmp_file = open(tmp_img, 'wb')
-tmp_file.seek(1024 * 1024 * (size - offset))
-tmp_file.write(b'\0')
-tmp_file.close()
+def error(msg):
+    print(f'{args[0]}: error: {msg}', file=sys.stderr)
+    sys.exit(1)
 
-proc = subprocess.run(['mkfs.fat', '-F', '32', tmp_img])
-if proc.returncode != 0:
-    sys.exit(proc.returncode)
+if __name__ == '__main__':
+    args = sys.argv
+    argc = len(args)
 
-output_file = open(output_img, 'r+b')
-output_file.seek(1024 * 1024 * offset)
-tmp_file = open(tmp_img, 'rb')
-output_file.write(tmp_file.read())
-output_file.close()
-tmp_file.close()
-os.remove(tmp_img)
+    if argc < 1:
+        sys.exit(1)
 
-proc = subprocess.run([parted, '-s', output_img, '--', 'mklabel', 'msdos', \
-    'mkpart', 'primary', 'fat32', offset_s + 'MB', '-1s', 'set', '1', 'boot', 'on'])
-if proc.returncode != 0:
-    sys.exit(proc.returncode)
+    kernel = None
+    i = 1
 
-proc = subprocess.run([limine, 'bios-install', output_img])
-if proc.returncode != 0:
-    sys.exit(proc.returncode)
+    while i < argc:
+        arg = args[i]
+        if arg[0] == '-':
+            if len(arg) < 3 or arg[1] != '-':
+                error(f'unrecognized flag: \'{arg}\'')
+            split = arg[2:].split('=', maxsplit=1)
+            flag_name = split[0]
+            value = None
+            if len(split) == 1:
+                if i + 1 >= argc:
+                    error(f'expected value after flag \'{arg}\'')
+                value = args[i + 1]
+                i += 1
+            else:
+                value = split[1]
+            flag = None
+            for key in flags:
+                _flag = flags[key]
+                if _flag.long_name == flag_name:
+                    flag = _flag
+                    break
+            if flag is None:
+                error(f'unrecognized flag: \'--{flag_name}\'')
+            flag.value = value
+        elif kernel is None:
+            kernel = arg
+        else:
+            print(kernel)
+            usage()
+        i += 1
 
-def mcopyfile(src, dst):
-    proc = subprocess.run([mcopy, '-i', output_img + '@@' + offset_s + 'M', src, dst])
+    if kernel is None:
+        usage()
+
+    parted = flags['parted'].value
+    limine = flags['limine'].value
+    mcopy = flags['mcopy'].value
+    mmd = flags['mmd'].value
+    limine_bios_sys = flags['limine-bios-sys'].value
+    limine_conf = flags['limine-conf'].value
+    output = flags['output'].value
+    mkfs = 'mkfs.fat'
+
+    output_file = open(output, 'wb')
+    output_file.seek(1024 * 1024 * OFFSET - 1)
+    output_file.write(b'\0')
+    output_file.close()
+
+    tmp_file = open(TMP, 'wb')
+    tmp_file.seek(1024 * 1024 * (SIZE - OFFSET) - 1)
+    tmp_file.write(b'\0')
+    tmp_file.close()
+
+    proc = subprocess.run([mkfs, '-F', '32', TMP])
     if proc.returncode != 0:
         sys.exit(proc.returncode)
 
-proc = subprocess.run([mmd, '-i', output_img + '@@' + offset_s + 'M', '::/boot'])
-if proc.returncode != 0:
-    sys.exit(proc.returncode)
+    output_file = open(output, 'r+b')
+    output_file.seek(1024 * 1024 * OFFSET)
+    tmp_file = open(TMP, 'rb')
+    output_file.write(tmp_file.read())
+    output_file.close()
+    tmp_file.close()
+    os.remove(TMP)
 
-mcopyfile(limine_bios_sys, '::/boot/')
-mcopyfile(limine_conf, '::/boot/')
-mcopyfile(kernel_elf, '::/boot/')
+    proc = subprocess.run([parted, '-s', output, '--', 'mklabel', 'msdos', \
+        'mkpart', 'primary', 'fat32', OFFSET_S + 'MB', '-1s', 'set', '1', 'boot', 'on'])
+    if proc.returncode != 0:
+        sys.exit(proc.returncode)
 
-sys.exit(0)
+    proc = subprocess.run([limine, 'bios-install', output])
+    if proc.returncode != 0:
+        sys.exit(proc.returncode)
+
+    def mcopyfile(src, dst):
+        proc = subprocess.run([mcopy, '-i', output + '@@' + OFFSET_S + 'M', src, dst])
+        if proc.returncode != 0:
+            sys.exit(proc.returncode)
+
+    proc = subprocess.run([mmd, '-i', output + '@@' + OFFSET_S + 'M', '::/boot'])
+    if proc.returncode != 0:
+        sys.exit(proc.returncode)
+
+    mcopyfile(limine_bios_sys, '::/boot/')
+    mcopyfile(limine_conf, '::/boot/')
+    mcopyfile(kernel, '::/boot/')
+
+    sys.exit(0)
